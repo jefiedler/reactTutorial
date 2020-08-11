ae186ac5dc96c2eabb9c165e2639dee5cbc069c2
jefiedler <jonas.fiedler@magenta.de>
2020-08-11 15:55:29 +0200

    Lifting State Up tutorial react

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,30 @@
 import "./board.css";
-import React from "react";
+import React, { useCallback } from "react";
 import Square from "./Square";
 
 export default function Board() {
   const status = "Next player: X";
+  const [squares, setSquares] = React.useState([null]);
+
+  const handleClick = (index) => {
+    const square = squares.slice();
+    square[index] = "🙉";
+    setSquares(square);
+  };
 
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board">
-        <Square value="0" />
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-        <Square value="7" />
-        <Square value="8" />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,16 +1,16 @@
 import "./square.css";
 import React from "react";
 
-export default function Square() {
+export default function Square(props) {
   const [value, setValue] = React.useState(null);
   return (
     <button
       className="square"
       onClick={() => {
-        setValue("X");
+        props.onClick();
       }}
     >
-      {value}
+      {props.value}
     </button>
   );
 }
